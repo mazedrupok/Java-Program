28e98fc1fbdcaf020e82bcb8a4d8706c49aee3ae
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2016-03-16 11:39:00 +0600

    Create input from scanner & print.java

---
+++ input from scanner & print.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class sample {
+	public static void main (String arg[]) {
+		System.out.println("Hello World");
+		int a, b, c;
+		Scanner cal = new Scanner (System.in);
+		a = cal.nextInt();
+		b = cal.nextInt();
+		c = a+b;
+		System.out.print("Total is: ");
+		System.out.println(c);
+		
+	}
+}
