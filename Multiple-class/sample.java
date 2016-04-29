930f7c93dbfc37d6f9d809cf77cf299cce33af4f
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2016-04-29 19:40:01 +0600

    Create sample.java

---
+++ Multiple-class/sample.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class sample {
+public static void main (String[] args) 
+{
+	other obj = new other();
+	obj.test();
+	System.out.println ("Hi. its from main");
+	
+}
+
+}
