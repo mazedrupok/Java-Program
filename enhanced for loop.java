98b29a431cd3410a7720312222cb08f5a42fe506
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2016-04-29 20:56:25 +0600

    enhanced for loop

---
+++ enhanced for loop.java
@@ -0,0 +1,13 @@
+
+public class sample {
+public static void main (String[] args) 
+{
+	int mazed[] = {3, 4, 5, 6, 7};
+	int sum = 0;
+	for (int x : mazed) {
+		sum += x;
+	}
+	System.out.println(sum);
+}
+
+}
