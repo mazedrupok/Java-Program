669a8f4757e1b59d1639dcd14584f46f0fce64a6
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2016-05-01 17:56:04 +0600

    toString and composition

---
+++ toString-and-composition/sampleMain.java
@@ -0,0 +1,11 @@
+
+public class sampleMain {
+public static void main (String[] args) 
+{
+	aDate birthday = new aDate (5, 10, 1994);
+	people peopleobj = new people ("Mazed", birthday);
+	
+	System.out.println (peopleobj);
+}
+
+}
