3cd287391add9f9587a80a43df33ec0f8f8b0277
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2016-08-14 13:25:00 +0600

    Create for-each.java

---
+++ for-each.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Random;
+
+public class sample {
+
+	public static void main(String[] args) {
+		int ara[] = {2, 4, 1, 5, 3};
+		int sum = 0;
+		for (int x: ara) {
+			sum += x;
+			System.out.println(x);
+		}
+		System.out.println("Summetion is: " + sum);
+	}
+
+}
