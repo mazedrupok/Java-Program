3ffaaa7ec8329a672aa618af6bc54fb6c407d055
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2016-08-14 19:23:26 +0600

    Create sample.java

---
+++ Final-variable-initialize-by-constructor/sample.java
@@ -0,0 +1,12 @@
+
+public class sample {
+	public static void main(String[] args) {
+		people p = new people (10);
+		p.Add();
+		p.Add();
+		System.out.println(p);
+		people q = new people (20);
+		q.Add();
+		System.out.println(q);
+	}
+}
